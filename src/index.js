7676a4a3d0df34c171f7a90451f3feb7ad5e47ec
arturpilarek <artur.pilarek@gmail.com>
2021-10-11 11:42:46 +0200

    Template ready to use

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const UserList = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+ReactDOM.render(<UserList />, document.getElementById("app"))
